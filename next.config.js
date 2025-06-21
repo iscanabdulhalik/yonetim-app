f0ce35fb9fae2432ecb74cfbe1626d08db8c48d8
iscanabdulhalik <dev.haluk@gmail.com>
2025-06-22 00:20:28 +0300

    feat: Update next-intl to version 4.1.0 and adjust configuration for internationalization

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const createNextIntlPlugin = require("next-intl/plugin");
-const withNextIntl = createNextIntlPlugin("./src/i18n.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    _next_intl_trailing_slash: "false",
+  },
   images: {
     domains: ["localhost", "vercel.app"],
     remotePatterns: [
@@ -43,4 +45,5 @@ const nextConfig = {
   },
 };
 
+const withNextIntl = createNextIntlPlugin("./src/i18n.ts");
 module.exports = withNextIntl(nextConfig);
